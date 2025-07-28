bcfe07858e2ba73932700e4bdf6f732fac7f6f71
Blank <58242347+TheBlankness@users.noreply.github.com>
2025-07-28 09:24:52 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
+import fg from 'fast-glob'; // 1️⃣ new import
+
+// 2️⃣ grab all .css files in resources/css/
+const cssFiles = fg.sync('resources/css/**/*.css');
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css','resources/css/customerlogin.css' ,'resources/js/app.js'],
+            input: [
+                ...cssFiles, // 3️⃣ inject all CSS files
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
         tailwindcss(),
